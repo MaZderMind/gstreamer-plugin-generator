d52ef37ef474dedee999181b9314780b1e9a6880
Peter Körner <pkoerner@seibert-media.net>
2019-01-21 23:50:50 +0100

    add build-tests for all property-types

--- buildtest/test_cases.py
+++ buildtest/test_cases.py
@@ -5,6 +5,31 @@ TEST_NAME = 'Test'
 
 test_cases = {
 	"default_config": build_plugin_config(),
+	"multiple_elements": build_plugin_config(
+		elements=[
+			build_element_config(name="Test 1"),
+			build_element_config(name="Test 2"),
+			build_element_config(name="Test 3"),
+		]
+	),
+	"property_types": build_plugin_config(elements=[
+		build_element_config(properties=[
+			build_property_config(type='STRING', name='STRING-Property'),
+			build_property_config(type='BOOLEAN', name='BOOLEAN-Property'),
+			build_property_config(type='CHAR', name='CHAR-Property'),
+			build_property_config(type='UCHAR', name='UCHAR-Property'),
+			build_property_config(type='INT', name='INT-Property'),
+			build_property_config(type='UINT', name='UINT-Property'),
+			build_property_config(type='LONG', name='LONG-Property'),
+			build_property_config(type='ULONG', name='ULONG-Property'),
+			build_property_config(type='INT64', name='INT64-Property'),
+			build_property_config(type='UINT64', name='UINT64-Property'),
+			build_property_config(type='FLOAT', name='FLOAT-Property'),
+			build_property_config(type='DOUBLE', name='DOUBLE-Property'),
+			build_property_config(type='ENUM', name='ENUM-Property'),
+			build_property_config(type='FLAGS', name='FLAGS-Property'),
+		]),
+	]),
 	"same_name": build_plugin_config(
 		name=TEST_NAME,
 		description=TEST_NAME,
