5f4ab8d822b191850bbd496e81d2ea5890548d02
Peter Körner <pkoerner@seibert-media.net>
2019-02-04 01:21:05 +0100

    test http response to bad input json

--- lib/http_test.py
+++ lib/http_test.py
@@ -11,21 +11,31 @@ test_client = app.test_client()
 class TestPluginZipGenerator(unittest.TestCase):
 	def test_root(self):
 		response = test_client.get('/')
-		data = response.data
 		self.assertEqual(response.headers['Content-Type'], 'text/html; charset=utf-8')
 		self.assertGreater(int(response.headers['Content-Length']), 0)
-		self.assertEqual(len(data), int(response.headers['Content-Length']))
+		self.assertEqual(len(response.data), int(response.headers['Content-Length']))
 
 	def test_generate_json_post(self):
 		response = test_client.post('/generate', json=build_plugin_config())
-		data = response.data
 		self.assertEqual(response.headers['Content-Type'], 'application/zip')
 		self.assertEqual(response.headers['Content-Disposition'], 'attachment; filename=AwsomePlugin.zip')
-		self.assertGreater(len(data), 0)
+		self.assertGreater(len(response.data), 0)
 
 	def test_generate_json_arg(self):
 		response = test_client.post('/generate', data={'json': json.dumps(build_plugin_config())})
-		data = response.data
 		self.assertEqual(response.headers['Content-Type'], 'application/zip')
 		self.assertEqual(response.headers['Content-Disposition'], 'attachment; filename=AwsomePlugin.zip')
-		self.assertGreater(len(data), 0)
+		self.assertGreater(len(response.data), 0)
+
+	def test_rejects_incorrect_json(self):
+		response = test_client.post('/generate', data={'json': json.dumps({"name": "Incomplete"})})
+		self.assertEqual(response.headers['Content-Type'], 'text/html; charset=utf-8')
+		self.assertEqual(response.status, '400 BAD REQUEST')
+
+	def test_incorrect_json_shows_expected_error_page(self):
+		response = test_client.post('/generate', data={'json': json.dumps({"name": "Incomplete"})})
+		body = bytes(response.data).decode('utf-8')
+
+		self.assertIn('https://github.com/MaZderMind/gstreamer-plugin-generator/issues/new', body)
+		self.assertIn('&#39;description&#39; is a required property', body)
+		self.assertIn('"name": "Incomplete"', body)
